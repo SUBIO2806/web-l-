97879592e0cd58a7dd70da148c35de5ad96def22
SUBIO2806 <nnlam280603@gmail.com>
2024-11-07 20:43:22 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,217 @@
+        <?php include('allhead.php'); ?>
+        <!-- Page Content -->
+        <div class="container" style="max-width: 1200px;">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h1 class="page-header">About
+        <small>Cloud Classroom</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href=" index">Home</a>
+        </li>
+        <li class="active">About</li>
+     </ol>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+        <div class="col-md-6">
+        <img class="img-responsive" src="images/7fb1f193435815a86c8484f82b9589e1 - Copy.jpg" alt="">
+        </div>
+        <div class="col-md-6">
+        <h2>ABOUT CLOUD CLASSROOMS</h2>
+        <p>The <strong>“Cloud Classrooms”</strong> Website (web based application) is useful for the
+students, faculty, guest whomever likes to learn from web using E-Learn
+(Videos), as well Check result, schedules of assessment and all that task like
+event, news, students can find out list of fresh courses offered by them and
+admission procedure, discussion forum, fee structure etc. without going to
+institute. It provides the facility to the students or guest to have complete
+information about the institute.</p><br>
+<p>
+In this application, student can attend his\her missed classes from e-learn.</p>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h2 class="page-header">Our Teacher</h2>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/Untitled design (1).jpg" alt="">
+        <div class="caption">
+        <h3>Nguyen Ngoc Lam<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Our Computer teacher is my favorite teacher..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/IMG_9436 (1).jpg" alt="">
+        <div class="caption">
+        <h3>Nguyen Quoc Tien<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Because of my Science Teacher, Science has become my favorite subject too..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/IMG_9436 (1).jpg" alt="">
+        <div class="caption">
+        <h3>Nguyen Trung Kien<br>
+            <small>Teacher</small>
+        </h3>
+        <p>No one in our class has failed in Science because of his teaching.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/IMG_9437 (1).jpg" alt="">
+        <div class="caption">
+        <h3>Nguyen Van Vu<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Our Science teacher is my favorite teacher.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/IMG_9436 (1).jpg" alt="">
+        <div class="caption">
+        <h3> Mr. Hai<br>
+            <small>Teacher</small>
+        </h3>
+        <p>He is very kind to all the students.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/IMG_9436 (1).jpg" alt="">
+        <div class="caption">
+        <h3>Mr. Beo<br>
+            <small>Teacher</small>
+        </h3>
+        <p>He teaches the lessons every day and keeps tests.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Our Student's</h2>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/welcome46.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/smilingFemaleStudent.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Returning-Student.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Hopeful_student_1.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/happy_student4.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/tải xuống.jpg" alt="">
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+
+        <p class="footer text-center">Copyright &copy; Cloud Classroom 2024  <strong>BTL CSDL</strong> project Made by <strong>Bed Boys Team 007</strong></p>
+
+        </footer>
+
+        </div>
+        <!-- /.container -->
+        <style>
+
+        .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
+
+        </style>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script to Activate the Carousel -->
+        <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+        </script>
+
+        </body>
+        </html>
